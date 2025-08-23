e7d6a24824cd445ae23e86829a65884ba79581cf
AbdoHerO <abdo.sbeta.12@gmail.com>
2025-08-23 23:28:56 +0100

    Parrainage (Referral Links) ... 3

--- resources/ts/plugins/axios.ts
+++ resources/ts/plugins/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Create axios instance with default config
 const axiosInstance = axios.create({
-  baseURL: '/',
+  baseURL: '/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
