1c31d0b88283b50d71d067e09d85012d7f2cedb0
AbdoHerO <abdo.sbeta.12@gmail.com>
2025-08-31 16:42:03 +0100

    rooleback vite

--- vite.config.ts
+++ vite.config.ts
@@ -11,33 +11,19 @@ import { defineConfig } from 'vite'
 import MetaLayouts from 'vite-plugin-vue-meta-layouts'
 import vuetify from 'vite-plugin-vuetify'
 import svgLoader from 'vite-svg-loader'
-import { createSubfolderPlugin } from './vite-plugins/subfolder-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tujjar/',
-  build: {
-    chunkSizeWarningLimit: 5000,
-    rollupOptions: {
-      output: {
-        // Ensure dynamic imports use correct paths
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-        manualChunks: undefined,
-      },
-    },
-  },
-  plugins: [
-    // Docs: https://github.com/posva/unplugin-vue-router
-    // ℹ️ This plugin should be placed before vue plugin
+  plugins: [// Docs: https://github.com/posva/unplugin-vue-router
+  // ℹ️ This plugin should be placed before vue plugin
     VueRouter({
       getRouteName: routeNode => {
-        // Convert pascal case to kebab case
+      // Convert pascal case to kebab case
         return getPascalCaseRouteName(routeNode)
           .replace(/([a-z\d])([A-Z])/g, '$1-$2')
           .toLowerCase()
       },
+
       routesFolder: 'resources/ts/pages',
     }),
     vue({
@@ -45,6 +31,7 @@ export default defineConfig({
         compilerOptions: {
           isCustomElement: tag => tag === 'swiper-container' || tag === 'swiper-slide',
         },
+
         transformAssetUrls: {
           base: null,
           includeAbsolute: false,
@@ -54,30 +41,29 @@ export default defineConfig({
     laravel({
       input: ['resources/ts/main.ts'],
       refresh: true,
-      buildDirectory: 'build',
+      buildDirectory: 'build', // Prevent /build prefix
     }),
-    createSubfolderPlugin('/tujjar/'),
-    vueJsx(),
+    vueJsx(), // Docs: https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin
     vuetify({
       styles: {
         configFile: 'resources/styles/variables/_vuetify.scss',
       },
-    }),
+    }), // Docs: https://github.com/dishait/vite-plugin-vue-meta-layouts?tab=readme-ov-file
     MetaLayouts({
       target: './resources/ts/layouts',
       defaultLayout: 'default',
-    }),
+    }), // Docs: https://github.com/antfu/unplugin-vue-components#unplugin-vue-components
     Components({
       dirs: ['resources/ts/@core/components', 'resources/ts/views/demos', 'resources/ts/components'],
       dts: true,
       resolvers: [
         componentName => {
-          // Auto import `VueApexCharts`
+        // Auto import `VueApexCharts`
           if (componentName === 'VueApexCharts')
             return { name: 'default', from: 'vue3-apexcharts', as: 'VueApexCharts' }
         },
       ],
-    }),
+    }), // Docs: https://github.com/antfu/unplugin-auto-import#unplugin-auto-import
     AutoImport({
       imports: ['vue', VueRouterAutoImports, '@vueuse/core', '@vueuse/math', 'vue-i18n', 'pinia'],
       dirs: [
@@ -88,9 +74,10 @@ export default defineConfig({
         './resources/ts/plugins/*/composables/*',
       ],
       vueTemplate: true,
+
       // ℹ️ Disabled to avoid confusion & accidental usage
       ignore: ['useCookies', 'useStorage'],
-    }),
+    }), // Docs: https://github.com/intlify/bundle-tools/tree/main/packages/unplugin-vue-i18n#intlifyunplugin-vue-i18n
     VueI18nPlugin({
       runtimeOnly: true,
       compositionOnly: true,
@@ -114,6 +101,9 @@ export default defineConfig({
       '@db': fileURLToPath(new URL('./resources/ts/plugins/fake-api/handlers/', import.meta.url)),
       '@api-utils': fileURLToPath(new URL('./resources/ts/plugins/fake-api/utils/', import.meta.url)),
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 5000,
   },
   optimizeDeps: {
     exclude: ['vuetify'],
