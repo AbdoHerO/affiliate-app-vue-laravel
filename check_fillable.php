fb2fbb5121e48f179589f37e1b393008d67e131c
AbdoHerO <abdo.sbeta.12@gmail.com>
2025-09-03 00:32:08 +0100

    Cleaning

--- check_fillable.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$model = new App\Models\AffiliateCartItem();
-echo "Fillable properties: " . json_encode($model->getFillable()) . "\n";
